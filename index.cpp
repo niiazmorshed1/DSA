88b59ae5875818aa865047bd96156bf56a3e6df2
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-12-27 00:12:18 +0600

    First Commit

---
+++ index.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// void myFunction(int a){
+//     int counter = 0;
+//     for (int i = 0; i <to_string(a).length(); i++) {
+//         int chk = floor(a/10);
+//         int a = chk ;
+//         counter += 1;
+//     }
+//     cout <<"The Number of Digits in The number is: " <<counter; 
+// }
+// int main(){
+// myFunction(34567895);
+// }
+
+
+// Palindrome Number
+#include <iostream>
+#include <cmath>
+using namespace std;
+void myFunction(string a){
+    for (int i = a.length()-1; i >= 0; i--) {
+        if ( )
+    }
+}
+int main(){
+myFunction("36344375");
+}
+
---
+++ index.js
@@ -0,0 +1,6 @@
+// function abc(a,b){
+//     sum = a+b;
+//     console.log(sum);
+// }
+// abc(6,4);
+console.log("Hello Sama")
